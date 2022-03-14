b7e7d68854ea2a5b326bb767a9897a10f40572e9
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-03-14 22:33:02 +0530

    Create division.java

---
+++ program-to-find-the-division-of-two-numbers/division.java
@@ -0,0 +1,17 @@
+//Program to find the division of two numbers
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner in = new Scanner(System.in);
+	    int x,y;
+	    float div;
+		System.out.print("Enter the dividend : ");
+		x=in.nextInt();
+		System.out.print("Enter th divisor : ");
+		y=in.nextInt();
+		div=x/y;
+		System.out.println(x+" / "+y+" =  "+div);
+	}
+}
