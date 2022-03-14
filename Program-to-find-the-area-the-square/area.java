369a8a8b6b053efd5eb0fe82808a41b7563b7067
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-03-14 22:39:12 +0530

    Create area.java

---
+++ Program-to-find-the-area-the-square/area.java
@@ -0,0 +1,14 @@
+//Program to find the area of square
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner in = new Scanner(System.in);
+	    int x,area=0;
+		System.out.print("Enter the side of the square : ");
+		x=in.nextInt();
+		area=x*x;
+		System.out.println("The area of the square is = "+area);
+	}
+}
