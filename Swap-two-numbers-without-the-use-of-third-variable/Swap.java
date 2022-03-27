6199ce023f82c53c177fcd16f2f7a3ace788b266
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-03-27 13:44:09 +0530

    Create Swap.java

---
+++ Swap-two-numbers-without-the-use-of-third-variable/Swap.java
@@ -0,0 +1,21 @@
+/*
+  program to swapp two number without the use of third variable
+*/
+import java.util.*;
+public class Swap 
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a,b,temp;
+        System.out.print("Enter the value in a :");
+        a=in.nextInt();
+        System.out.print("Enter the value in b :");
+        b=in.nextInt();
+        System.out.println("Values before swap:\na = "+a+"\nb = "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("Values after swap:\na = "+a+"\nb = "+b);;
+    }    
+}
