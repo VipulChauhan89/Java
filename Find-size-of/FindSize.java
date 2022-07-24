1b2aa75b673ecb515e739c1b8584ad9ad2093e00
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-07-24 14:02:14 +0530

    Create FindSize.java

---
+++ Find-size-of/FindSize.java
@@ -0,0 +1,11 @@
+public class FindSize
+{
+    public static void main(String args[])
+    {
+        // sizeof evaluates the size of a variable
+        System.out.println("Size of int: "+Integer.BYTES+" bytes");
+        System.out.println("Size of float: "+Float.BYTES+" bytes");
+        System.out.println("Size of double: "+Double.BYTES+" bytes");
+        System.out.println("Size of char: "+Character.BYTES+" bytes");
+    }
+}
